85c0370702e77c9707d1547def668b8efc493a25
github_username <wilsonzakk8@gmail.com>
2023-01-31 10:32:31 +1000

    misc

--- Models/Repository/CommentRepository.cs
+++ Models/Repository/CommentRepository.cs
@@ -23,38 +23,18 @@ namespace SQL_WEB_APPLICATION.Models.Repository
             var query = "INSERT INTO [Comments] (comment_text, created_date, email, fk_product_id) " +
                         "VALUES (@comment_text, @created_date, @email, @fk_product_id) ";
 
-
-            //var parameters = new DynamicParameters();
-            //parameters.Add("comment_text", commentModel.comment_text, DbType.String);
-            //parameters.Add("created_date", commentModel.created_date, DbType.String);
-            //parameters.Add("fk_user_id", userModel.email, DbType.String);
-            //parameters.Add("fk_product_id", productModel.product_name, DbType.String);
-
-            //using (var connection = _context.CreateConnection())
-            //{
-            //    await connection.ExecuteAsync(query);
-            //}
-
             using (var connection = _context.CreateConnection())
             {
-
-                //commentDto.comment_text = commentModel.comment_text;
-                //commentDto.created_date = commentModel.created_date;
-                //userModel.email = commentModel.fk_user_id.ToString();
-                //productModel.product_name = commentModel.fk_product_id.ToString();
-
-                await connection.ExecuteAsync(query);
+                await connection.ExecuteAsync(query, commentModel);
             }
         }
 
         public async Task<IEnumerable<CommentModel>> GetComments()
         {
-            string query = "SELECT Comments.comment_id, Comments.comment_text, Comments.created_date, [User].email, " +
+            string query = "SELECT Comments.comment_id, Comments.comment_text, Comments.created_date, " +
                                      "Products.product_name " +
-                                     "FROM [User] INNER JOIN " +
-                                     "Comments ON [User].user_id = Comments.fk_user_id " +
-                                     "INNER JOIN " +
-                                     "Products ON Comments.fk_product_id = Products.product_id ";
+                                     "FROM Products  INNER JOIN " +
+                                     "Comments ON Products.product_id = Comments.fk_product_id ";
 
             using (var connection = _context.CreateConnection())
             {
