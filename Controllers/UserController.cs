404a53fc5c2c764e6f54f259ab7dba0652692e72
zakk wilson <Wilsonzakk8@gmail.com>
2023-02-26 22:09:07 +1000

    misc

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -18,72 +18,107 @@ namespace SQL_WEB_APPLICATION.Controllers
         #region Dapper intailized
         private readonly IUserRepository _userRepository;
 
-        public UserController(IUserRepository userRepository)
+        public UserController(IUserRepository user_repository)
         {
-            _userRepository = userRepository;
+            _userRepository = user_repository;
         }
         #endregion
 
         #region Email validation using regex
         static bool EmailValidation(string email)
         {
-            Regex emailValidation = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", RegexOptions.IgnoreCase);
-            return emailValidation.IsMatch(email);
+            Regex email_validation = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", RegexOptions.IgnoreCase);
+            return email_validation.IsMatch(email);
         }
         #endregion
 
+        #region Get user id
         [HttpGet]
         [Route("GetSessionId")]
         public async Task<IActionResult> GetSessionId()
         {
             string id = HttpContext.Session.Id;
-            
-            return Ok(id);
+
+            try
+            {
+                return Ok(id);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
         }
+        #endregion
 
         #region Authenticates user login
         [HttpGet]
         [Route("AuthenticateLogin")]
         [AutoValidateAntiforgeryToken]
-        public async Task<IActionResult> AuthenticateLogin(UserModel? userModel)
+        public async Task<IActionResult> AuthenticateLogin(UserModel? user_model)
         {
             string message;
-            var loginStatus = _userRepository.GetUsers().Result.Where(m => m.email.Trim() == userModel.email &&
-                                                                                          m.password.Trim() == userModel.password).FirstOrDefault();
-            var email_valadtion = EmailValidation(userModel.email);
+            var login_status = _userRepository.GetUsers().Result.Where(m => m.email.Trim() 
+                                                                      == user_model.email &&                    
+                                                                      m.password.Trim() 
+                                                                      == user_model.password).FirstOrDefault();
+            var email_valadtion = EmailValidation(user_model.email);
 
-            if (loginStatus != null & email_valadtion == true)
+            try
             {
-                string id = HttpContext.Session.Id;
-                HttpContext.Session.GetString(id);
-                message = "LOGIN VALID";
+                if (login_status == null)
+                {
+                    message = "LOGIN INVALID";
+                }
+
+                if (email_valadtion == false)
+                {
+                    message = "ENTER A EMAIL";
+                }
+
+                else
+                {
+                    string id = HttpContext.Session.Id;
+                    HttpContext.Session.GetString(id);
+                    message = "LOGIN VALID";
+                }
+                return Json(message);
             }
-            else
+            catch (Exception ex) 
             {
-                message = "LOGIN INVALID";
+                return Problem(ex.Message);
             }
-            return Json(message);
         }
         #endregion
 
         #region Checks user login
         [HttpGet]
         [Route("CheckLogin")]
-        public async Task<IActionResult> CheckLogin(UserModel? userModel)
+        public async Task<IActionResult> CheckLogin(UserModel? user_model)
         {
             string message;
-            var checkStatus = _userRepository.CheckUsers().Result.Where(m => m.email.Trim() == userModel.email &&
-                                                                                          m.password.Trim() == userModel.password).FirstOrDefault();
-            if (checkStatus != null)
+            var check_status = _userRepository.CheckUsers().Result.Where(m => m.email.Trim() 
+                                                                        == user_model.email &&
+                                                                        m.password.Trim() 
+                                                                        == user_model.password).FirstOrDefault();
+            try
             {
-                message = "LOGIN VALID";
-                return Ok(checkStatus.user_id);
+                if (check_status == null)
+                {
+                    message = "LOGIN INVALID";
+                }
+
+                else
+                {
+                    message = "LOGIN VALID";
+                    return Ok(check_status.user_id);
+                }
+
+                return Json(message);
             }
-            else
+            catch (Exception ex) 
             {
-                message = "LOGIN INVALID";
+                return Problem(ex.Message);
             }
-            return Json(message);
         }
         #endregion
 
@@ -93,49 +128,83 @@ namespace SQL_WEB_APPLICATION.Controllers
         public async Task<IActionResult> GetAllUsers()
         {
             var users = await _userRepository.GetAllUsers();
-            return Ok(users);
+
+            try
+            {
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
         }
         #endregion
 
         #region Checks if the inputed detials match an entry in the [User] table if so the creates a new user
         [HttpGet]
         [Route("CheckUser")]
-        public async Task<IActionResult> CheckUser(UserModel? userModel)
+        public async Task<IActionResult> CheckUser(UserModel? user_model)
         {
             string message;
-            var checkStatus = _userRepository.CheckUsers().Result.Where(m => m.email.Trim() == userModel.email).FirstOrDefault();
-            if (checkStatus == null)
+            var check_status = _userRepository.CheckUsers().Result.Where(m => m.email.Trim()
+                                                                        == user_model.email).FirstOrDefault();
+            var email_valadtion = EmailValidation(user_model.email);
+
+            try
             {
-                message = "LOGIN VALID";
-                await _userRepository.PostUser(userModel);
+                if (check_status == null)
+                {
+                    message = "LOGIN INVALID";
+                }
+
+                if (email_valadtion == false)
+                {
+                    message = "ENTER A EMAIL";
+                }
+
+                else
+                {
+                    message = "LOGIN VALID";
+                    await _userRepository.PostUser(user_model);
+                }
+
+                return Json(message);
             }
-            else
+            catch (Exception ex)
             {
-                message = "LOGIN INVALID";
+                return Problem(ex.Message);
             }
-            return Json(message);
         }
         #endregion
 
         #region Checks if the inputed detials match an entry in the [User] table if so the updates a users details
         [HttpPut]
         [Route("UpdateUser")]
-        public async Task<IActionResult> UpdateUser(UserModel userModel)
+        public async Task<IActionResult> UpdateUser(UserModel user_model)
         {
             string message;
-            var email = userModel.email;
+            var email = user_model.email;
             var email_valadation = EmailValidation(email);
 
-            if (email_valadation == true)
+            try
             {
-                message = "UPDATE COMPLETE";
-                await _userRepository.UpdateUser(userModel);
+                if (email_valadation == false)
+                {
+                    message = "UPDATE ERROR";
+                }
+
+                else
+                {
+                    message = "UPDATE COMPLETE";
+                    await _userRepository.UpdateUser(user_model);
+                }
+
+                return Json(message);
             }
-            else
+            catch (Exception ex)
             {
-                message = "UPDATE ERROR";
+                return Problem(ex.Message);
             }
-            return Json(message);
         }
         #endregion
 
@@ -144,8 +213,15 @@ namespace SQL_WEB_APPLICATION.Controllers
         [Route("DeleteUser")]
         public async Task<IActionResult> DeleteUser(UserModel id)
         {
-            await _userRepository.DeleteUser(id);
-            return Json("ACCOUNT DELETED");
+            try
+            {
+                await _userRepository.DeleteUser(id);
+                return Json("ACCOUNT DELETED");
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
         }
         #endregion
     }
